abe30d4889f22435c6565ecd10095fada6d60613
mattisos <mattis.osula@funkdigital.ee>
2023-02-08 18:15:20 +0200

    kodutöö parandused

--- matkaklubi/uudised.js
+++ matkaklubi/uudised.js
@@ -4,24 +4,28 @@ let uudised = [
         sisu: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores expedita maxime quas animi porro, dicta asperiores enim nihil fugit, rerum quaerat. Reiciendis quae aut, nisi illo inventore incidunt et non!',
         autor: 'Madis Maasikas',
         pildiAadress: './Assets/header image.png',
+        kuupaev: '07.02.2023',
     },
     {
         pealkiri: 'Uudis nr 2',
         sisu: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores expedita maxime quas animi porro, dicta asperiores enim nihil fugit, rerum quaerat. Reiciendis quae aut, nisi illo inventore incidunt et non!',
         autor: 'Jüri Juurikas',
         pildiAadress: './Assets/header image.png',
+        kuupaev: '07.02.2023',
     },
     {
         pealkiri: 'Uudis nr 3',
         sisu: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores expedita maxime quas animi porro, dicta asperiores enim nihil fugit, rerum quaerat. Reiciendis quae aut, nisi illo inventore incidunt et non!',
         autor: 'Lea Leevike',
         pildiAadress: './Assets/header image.png',
+        kuupaev: '07.02.2023',
     },
     {
         pealkiri: 'Uudis nr 4',
         sisu: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores expedita maxime quas animi porro, dicta asperiores enim nihil fugit, rerum quaerat. Reiciendis quae aut, nisi illo inventore incidunt et non!',
         autor: 'Madis Maasikas',
         pildiAadress: './Assets/header image.png',
+        kuupaev: '07.02.2023',
     },
 ]
 
@@ -35,6 +39,7 @@ function koostaUudiseHTML(uudis) {
              <p>
                 ${uudis.sisu}
              </p>
+             <p class="kp">${uudis.kuupaev}</p>
             </div>
         </div>
     </div>
